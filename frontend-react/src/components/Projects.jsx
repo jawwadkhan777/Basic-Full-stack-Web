08a76099bdb1fa607f51f5198322eaf8c643e2dd
Muhammad Jawwad Khan <m.jawwadkhan777@gmail.com>
2025-04-15 23:32:46 +0500

    Updating UI

--- frontend-react/src/components/Projects.jsx
+++ frontend-react/src/components/Projects.jsx
@@ -11,7 +11,7 @@ const Projects = ({ projects, onProjectDeleted, onProjectUpdated }) => {
     return (
         <ul className='grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2  gap-10 mt-10 mb-10'>
             {projects.map(proj => (
-                <li key={proj._id} className='list-none flex gap-2 text-lg items-center'>
+                <li key={proj._id} className='list-none border-1 rounded border-green-500 p-2 flex gap-2 text-lg items-center'>
                     <FontAwesomeIcon icon={faDiagramProject} className='text-green-500 ' />
                     <div className='flex items-center justify-between w-full'>
                         <p className=''>{proj.proj_name}</p>
